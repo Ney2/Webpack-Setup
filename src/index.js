0b383d10ab867ea38ddfbab24858750ed6c85a8f
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-07-13 00:32:59 +0300

    Add configuration for webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./main.css";
+const container = document.querySelector(".div");
+const newTitle = document.createElement('p');
+newTitle.textContent = "Setting up Webpack";
+container.appendChild(newTitle);
